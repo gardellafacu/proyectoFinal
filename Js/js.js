712b498f999a0b18323a69a1e8b6622e3d9aacd9
Facu Gardella <gardella.facundo@gmail.com>
2023-03-03 00:07:32 -0300

    editando

--- Js/js.js
+++ Js/js.js
@@ -1,8 +1,6 @@
 const body = document.querySelector('body'),
     sidebar = body.querySelector('nav'),
     toggle = body.querySelector(".toggle"),
-    searchBtn = body.querySelector(".search-box"),
-    modeSwitch = body.querySelector(".toggle-switch"),
     modeText = body.querySelector(".mode-text");
 
 
@@ -14,13 +12,3 @@ searchBtn.addEventListener("click", () => {
     sidebar.classList.remove("close");
 })
 
-modeSwitch.addEventListener("click", () => {
-    body.classList.toggle("dark");
-
-    if (body.classList.contains("dark")) {
-        modeText.innerText = "Light mode";
-    } else {
-        modeText.innerText = "Dark mode";
-
-    }
-});
